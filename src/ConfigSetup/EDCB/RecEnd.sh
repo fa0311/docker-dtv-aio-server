1870341f52d00bedbde09e772953504cd6ad965b
ふぁ <yuki@yuki0311.com>
2025-11-17 14:17:44 +0000

    add

--- src/ConfigSetup/EDCB/RecEnd.sh
+++ src/ConfigSetup/EDCB/RecEnd.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 #!_EDCBX_DIRECT_
 
-AmatsukazeAddTask -ip amatsukaze -p 32768 -s "デフォルト" -f "/record/TV-Record/${FileName}.ts" -o "/record/TV-Encode"
+curl -X POST http://file-transfer-server:8080/transfer \
+  -H "Content-Type: application/json" \
+  -d "{\"source_path\":\"local:/TV-Record/${FileName}.ts\",\"dest_path\":\"peer:/TV-Record/${FileName}.ts\"}"
+curl -X POST http://file-transfer-server:8080/transfer \
+  -H "Content-Type: application/json" \
+  -d "{\"source_path\":\"local:/TV-Record/${FileName}.ts.err\",\"dest_path\":\"peer:/TV-Record/${FileName}.ts.err\"}"
+curl -X POST http://file-transfer-server:8080/transfer \
+  -H "Content-Type: application/json" \
+  -d "{\"source_path\":\"local:/TV-Record/${FileName}.ts.program.txt\",\"dest_path\":\"peer:/TV-Record/${FileName}.ts.program.txt\"}"
+
+AmatsukazeAddTask -ip "192.168.70.2" -p 32768 -s "nvenc" -f "/record/TV-Record/${FileName}.ts" -o "/record/TV-Encode"
